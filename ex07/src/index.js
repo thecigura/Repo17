519cc73bc5caac8d0f16215b71806ca049879929
Denan <denan.cigura@gmail.com>
2021-09-29 11:17:04 +0200

    Ex 07 - setup

---
+++ ex07/src/index.js
@@ -0,0 +1,19 @@
+const range = {
+    max: 100.00,
+    min: -25.00,
+    num1: 17.50,
+    num2: 88.80,
+    average: 45.325
+};
+// Only change code below this line
+
+    
+const middle = (range) => (range.max + range.min) / 2.0; 
+
+
+// Only change code above this line
+
+
+
+console.log(removeFirstTwo(array1));
+module.exports = removeFirstTwo;
