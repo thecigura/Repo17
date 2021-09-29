8cf6e49b839a6eb37ed36901b009c726d673f1ed
Denan <denan.cigura@gmail.com>
2021-09-29 11:33:29 +0200

    Ex 08 - setup

---
+++ ex08/src/index.js
@@ -0,0 +1,33 @@
+// Lodash
+const _ = require ('lodash');
+
+// only change code below this line
+// users nested array with four objects starts here
+
+
+
+// users nested array with four objects ends here
+
+
+// getUsers function - list of users starts here
+
+
+
+// getUsers function - list of users ends here
+
+
+// findUser (lastName, gender) function starts here
+function findUser(lastName, gender) {
+    try {
+        // add appropriate code here
+    } catch (error) {
+    return // Change this line
+    console.log(); // Change this l i ne
+    }
+}
+
+// findUser(lastName, gender) function ends here
+// Only change code above this l ine
+getUsers();
+findUser(); // Change this line
+module.exports findUser ;
