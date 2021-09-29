d17602b2b2b84a7134f9ba29ea88b74cabd2d609
Denan <denan.cigura@gmail.com>
2021-09-29 11:38:29 +0200

    Ex 09 - setup

---
+++ ex09/src/index.js
@@ -0,0 +1,36 @@
+// Lodash
+const _ = require ('lodash');
+
+// only change code below this line
+// users nested array with four objects starts here
+
+
+
+// users nested array with four objects ends here
+
+
+// getUser function - list of users starts here
+
+
+
+// getUser function - list of users ends here
+
+
+// findUser(id) function starts here
+function findUserById(id) {
+    try {
+
+        // add appropriate code here
+
+    } catch (error) {
+    return // Change this line
+    console.log(); // Change this line
+    }
+}
+
+// findUserById(id) function ends here
+// Only change code above this line
+getUsers();
+findUserById(); // Change this line
+
+module.exports = findUserById;
