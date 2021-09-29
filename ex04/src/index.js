bda13f8be465c7dd29ffb0e5eaf7a3e4cc4ecd16
Denan <denan.cigura@gmail.com>
2021-09-29 10:52:22 +0200

    Ex 04 - setup

---
+++ ex04/src/index.js
@@ -0,0 +1,20 @@
+const BAKING_TEMPERATURES = {
+    cake: { low: 180, high: 250 },
+    bread: { low: 150, high: 220 },
+    pie: { low: 200, high: 230 },
+};
+
+function main(baking) {
+    // Only change code below this line
+    
+
+    const lowBread = BAKING_TEMPERATURES.bread.low;
+    const highBread = BAKING_TEMPERATURES.bread.high;
+
+    return {lowBread, highBread}
+
+    // Only change code above this line
+}
+
+console.log(main(BAKING_TEMPERATURES));
+module.exports = main;
