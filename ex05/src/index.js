304781bf3649e0f13ca879c313d54162491820ec
Denan <denan.cigura@gmail.com>
2021-09-30 08:39:33 +0200

    Ex05

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -3,7 +3,7 @@ let foo = 12, boo = 45;
 function main(foo, boo) {
     // Only change code below this line
     
-
+   [foo, boo] = [boo, foo];
     // Only change code above this line
     return {foo, boo}
 }
