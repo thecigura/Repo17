d6ed515fedf0722bb438811e0473257edf4cda31
Denan <denan.cigura@gmail.com>
2021-09-30 08:39:58 +0200

    Ex06

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -3,8 +3,8 @@ const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 function removeFirstTwo(list) {
     "use strict"
     // Only change code below this line
-    
-    const array2 = list; // Change this line
+
+    const [a, b, ...array2] = list; // Change this line
 
     // Only change code above this line
     return array2;
